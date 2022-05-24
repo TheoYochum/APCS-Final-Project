9a6ec596cf6af0b8eb8098b3d8db8e41239f5749
Theo Yochum <theoyochum@gmail.com>
2022-05-24 09:29:28 -0400

    Strucutre of List and Point

--- Project/Point.java
+++ Project/Point.java
@@ -1,3 +1,20 @@
 public class Point extends Variable {
-  
+  private Number xCor;
+  private Number yCor;
+
+  public Number getX() {
+    return xCor;
+  }
+
+  public Number getY() {
+    return yCor;
+  }
+
+  public void setX(Number in) {
+    xCor = in;
+  }
+
+  public void setY(Number in) {
+    yCor = in;
+  }
 }
