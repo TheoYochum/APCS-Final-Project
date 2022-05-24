9acb3bd537d6ed49c80e017fb391a4e13e1479a9
pharkoah <akram.kh.125@gmail.com>
2022-05-24 13:22:13 -0400

    calc skeleton

--- Project/Calculator.java
+++ Project/Calculator.java
@@ -1,3 +1,21 @@
+import java.lang.*;
+import java.util.*;
+import java.io.*;
 public class Calculator {
-  
+  private Scanner input;
+  private HashMap<Variable>;
+
+  public void display() {
+
+  }
+
+  public boolean addVariable(String str) {
+
+  }
+
+  public Number expressionParse() {
+
+  }
+
+  //add graphing function
 }
