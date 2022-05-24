9371e0f15aa7b8a4a4520a855f2b7bf30bc9d686
Theo Yochum <theoyochum@gmail.com>
2022-05-24 15:14:49 -0400

    Added point and fraction parsing

--- Project/Calculator.java
+++ Project/Calculator.java
@@ -37,12 +37,17 @@ public class Calculator {
   private void newVariable() {
     int intValue;
     double doubleValue;
+    Int numerator;
+    Int denominator;
+    Number xCor;
+    Number yCor;
     String name;
     System.out.println("Type:");
     String type = input.nextLine();
+    type = type.toLowerCase();
     Display.clear();
     switch (type) {
-      case "Integer":
+      case "integer":
         System.out.println("Name:");
         name = input.nextLine();
         Display.clear();
@@ -51,7 +56,7 @@ public class Calculator {
         Display.clear();
         Variables.put(name, new Int(intValue, name));
         break;
-      case "Float":
+      case "float":
         System.out.println("Name:");
         name = input.nextLine();
         Display.clear();
@@ -60,12 +65,92 @@ public class Calculator {
         Display.clear();
         Variables.put(name, new Float(doubleValue, name));
         break;
+      case "fraction":
+        System.out.println("Name:");
+        name = input.nextLine();
+        Display.clear();
+        System.out.println("Numerator:");
+        numerator = new Int(input.nextInt(), name + " Numerator");
+        System.out.println("Denominator:");
+        denominator = new Int(input.nextInt(), name + " Denominator");
+        Display.clear();
+        Variables.put(name, new Fraction(numerator, denominator, name));
+        break;
+      case "point":
+        System.out.println("Name:");
+        name = input.nextLine();
+        Display.clear();
+        System.out.println("X Coordinate:");
+        xCor = newNumber();
+        System.out.println("Y Coordinate:");
+        yCor = newNumber();
+        Display.clear();
+        Variables.put(name, new Point(xCor, yCor, name));
+        break;
     }
+  }
+
+  private Number newNumber() {
+    Number out;
+    String name;
+    int intValue;
+    double doubleValue;
+    Int numerator;
+    Int denominator;
+    System.out.println("What type of Number");
+    String type = input.nextLine();
+    type = type.toLowerCase();
+    switch(type) {
+      case "integer":
+        System.out.println("Name:");
+        name = input.nextLine();
+        Display.clear();
+        System.out.println("Value:");
+        intValue = input.nextInt();
+        Display.clear();
+        out = new Int(intValue, name);
+        Variables.put(name, (Variable) out);
+        return out;
+      case "float":
+        System.out.println("Name:");
+        name = input.nextLine();
+        Display.clear();
+        System.out.println("Value:");
+        doubleValue = input.nextDouble();
+        Display.clear();
+        out = new Float(doubleValue, name);
+        Variables.put(name, (Variable) out);
+        return out;
+      case "fraction":
+        System.out.println("Name:");
+        name = input.nextLine();
+        Display.clear();
+        System.out.println("Numerator:");
+        numerator = new Int(input.nextInt(), name + " Numerator");
+        System.out.println("Denominator:");
+        denominator = new Int(input.nextInt(), name + " Denominator");
+        Display.clear();
+        out = new Fraction(numerator, denominator, name);
+        Variables.put(name, (Variable) out);
+        return out;
+    }
+    return null;
   }
 
   private void printVariable() {
     System.out.println("Name:");
-    System.out.println(Variables.get(input.nextLine()).value());
+    String name = input.nextLine();
+    if (!Variables.containsKey(name)) {
+      System.out.println("Not a valid variable name");
+      Display.clear();
+      return;
+    }
+    System.out.println(Variables.get(name).value());
+    hold();
+  }
+
+  private void hold() {
+    input.nextLine();
   }
 
   public static void main(String[] args) {
