8a06ad0150a82df4d180d9c38635b1b68f4e8304
pharkoah <akram.kh.125@gmail.com>
2022-05-25 10:09:52 -0400

    skeleton functons for Statistics

---
+++ Project/Statistics.java
@@ -0,0 +1,25 @@
+public class Statistics extends Functions {
+  public Number abs(Number x) {
+
+  }
+
+  public float ceil(Float x) {
+
+  }
+
+  public float floor(Float x) {
+
+  }
+
+  public Number log(Number x, Number y) {
+
+  }
+
+  public Int gcd(Int x, Int y) {
+
+  }
+
+  public Int lcm (Int x, Int y) {
+    
+  }
+}
