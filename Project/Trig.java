abad29d3f22081a8739c11dc9400373318826f13
Theo Yochum <theoyochum@gmail.com>
2022-05-26 16:56:54 -0400

    Added input contraints to trigonometric

--- Project/Trig.java
+++ Project/Trig.java
@@ -5,9 +5,10 @@
  * series function for arctan
  */
 public class Trig extends Functions {
-      
+    
   public static Float sin(Angle in) {
     double val = in.value();
+    val = constrain(val, in.isDegrees());
     if (in.isDegrees()) {
       val = Angle.degToRad(val);
     }
@@ -33,6 +34,7 @@ public class Trig extends Functions {
 
   public static Float cos(Angle in) {
     double val = in.value();
+    val = constrain(val, in.isDegrees());
     if (in.isDegrees()) {
       val = 90.0 - val;
     } else {
@@ -44,30 +46,34 @@ public class Trig extends Functions {
 
   public static Float tan(Angle in) {
     double val = sin(in).value() / cos(in).value();
+    val = constrain(val, in.isDegrees());
     return new Float(val, in.name() + " tangent");
   }
 
   public static Float csc(Angle in) {
     double val = 1 / sin(in).value();
+    val = constrain(val, in.isDegrees());
     return new Float(val, in.name() + " secant");
   }
 
   public static Float sec(Angle in) {
     double val = 1 / cos(in).value();
+    val = constrain(val, in.isDegrees());
     return new Float(val, in.name() + " cosecant");
   }
 
   public static Float cot(Angle in) {
     double val = 1 / tan(in).value();
+    val = constrain(val, in.isDegrees());
     return new Float(val, in.name() + " cotangent");
   }
 
-  public static Angle experimentalArctan(Float in, boolean isDegrees) {
+  public static Angle arctan(Float in, boolean isDegrees) {
     double val = in.value();
     double sum = 0.0;
     double temp;
     int seq;
-    for (int i = 1; i <= 10; i++) {
+    for (int i = 1; i <= 25; i++) {
       seq = (i - 1) * 2 + 1;
       temp = 0;
       temp += Math.pow(val, seq);
@@ -77,34 +83,43 @@ public class Trig extends Functions {
       }
       sum += temp;
     }
+    if (isDegrees) {
+      sum = Angle.radToDeg(sum);
+    }
     return new Angle(sum, isDegrees, in.name() + " arctangent");
   }
 
-
-  public static Angle arctan(Float in, boolean isDegrees) {
-    double val = Angle.radToDeg(Math.atan(in.value()));
-    return new Angle(val, isDegrees, in.name() + " arctangent");
-  }
-
   public static Angle arccos(Float in, boolean isDegrees) {
+    if (in.value() > 1 || in.value() < -1) {
+      throw new IllegalArgumentException("Input is not between -1 and 1");
+    }
     Float shfited = new Float(Math.sqrt(1 - Math.pow(in.value(), 2)) / in.value(), in.name() + " shifted");
     double val = arctan(shfited, isDegrees).value();
     return new Angle(val, isDegrees, in.name() + " arccosine");
   }
 
   public static Angle arcsin(Float in, boolean isDegrees) {
+    if (in.value() > 1 || in.value() < -1) {
+      throw new IllegalArgumentException("Input is not between -1 and 1");
+    }
     Float shfited = new Float(in.value() / Math.sqrt(1 - Math.pow(in.value(), 2)), in.name() + " shifted");
     double val = arctan(shfited, isDegrees).value();
     return new Angle(val, isDegrees, in.name() + " arcsine");
   }
 
   public static Angle arcsec(Float in, boolean isDegrees) {
+    if (in.value() < 1 && in.value() > -1) {
+      throw new IllegalArgumentException("Inputs between -1 and 1 are not valid");
+    }
     Float shfited = new Float(1 / in.value(), in.name() +  " shifted");
     double val = arccos(shfited, isDegrees).value();
     return new Angle(val, isDegrees, in.name() + " arcsecant");
   }
 
   public static Angle arccsc(Float in, boolean isDegrees) {
+    if (in.value() < 1 && in.value() > -1) {
+      throw new IllegalArgumentException("Inputs between -1 and 1 are not valid");
+    }
     Float shfited = new Float(1 / in.value(), in.name() +  " shifted");
     double val = arcsin(shfited, isDegrees).value();
     return new Angle(val, isDegrees, in.name() + " arccosecant");
@@ -114,5 +129,21 @@ public class Trig extends Functions {
     Float shfited = new Float(1 / in.value(), in.name() +  " shifted");
     double val = arctan(shfited, isDegrees).value();
     return new Angle(val, isDegrees, in.name() + " arctangent");
+  }
+
+  private static double constrain(double in, boolean isDegrees) {
+    double out = 0.0;
+    if (isDegrees) {
+      out = in % 360.0;
+      if (out < 0) {
+        out = 360 + out;
+      }
+    } else {
+      out = in % (pi * 2);
+      if (out < 0) {
+        out = 2 * pi + out;
+      }
+    }
+    return out;
   }
 }
