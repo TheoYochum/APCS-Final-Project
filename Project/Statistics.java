cca2421e5d511b6d36185ba99868e6a5f2c4be24
pharkoah <akram.kh.125@gmail.com>
2022-05-26 10:09:13 -0400

    floor and ceil return Int instead of Float now (also simplified their code)

--- Project/Statistics.java
+++ Project/Statistics.java
@@ -1,29 +1,27 @@
 public class Statistics extends Functions {
-  /*public static Number abs(Number x) {
+  public static Number abs(Number x) {
     if (x.value() < 0 ) {
-      return x.value() * -1;
-    }
-    return x.value();
-  }*/
-
-  public static double ceil(Float x) {
-    Int k = x.getInt();
-    if (x.value() < 0) {
-      x.setValue(k.intValue());
+      x.setValue(x.value() * -1);
     } else {
-      x.setValue(k.intValue() + 1);
+      x.setValue(x.value());
     }
-    return x.value();
+    return x;
   }
 
-  public static double floor(Float x) {
+  public static Int ceil(Float x) {
+    Int k = x.getInt();
+    if (x.value() > 0) {
+      k.setValue(k.intValue() + 1);
+    }
+    return k;
+  }
+
+  public static Int floor(Float x) {
     Int k = x.getInt();
     if (x.value() < 0) {
-      x.setValue(k.intValue() - 1);
-      return x.value();
+      k.setValue(k.intValue() - 1);
     }
-    x.setValue(k.intValue());
-    return x.value();
+    return k;
   }
 
   /*public static Number log(Number x, Number y) {
@@ -38,12 +36,12 @@ public class Statistics extends Functions {
 
   }*/
 
-  /*
+
   public static void main(String[] args) {
     Float x = new Float(1.22, "x");
     Float y = new Float(-420.0484838, "y");
-    floor(x);
-    floor(y);
+    System.out.println(ceil(x));
+    System.out.println(ceil(y));
     System.out.println(x + " " + y);
-  }*/
+  }
 }
