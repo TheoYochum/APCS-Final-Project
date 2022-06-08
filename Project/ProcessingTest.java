35518f50f723149dccb31b26246b7697deab901a
Theo Yochum <theoyochum@gmail.com>
2022-06-07 20:24:03 -0400

    Processing running from terminal

---
+++ Project/ProcessingTest.java
@@ -0,0 +1,24 @@
+import processing.core.PApplet;
+
+// Compile with: javac -cp ./core.jar ./ProcessingTest.java ProcessingTest.java
+// Run with: javac -cp ./core.jar ./ProcessingTest.java ProcessingTest.java
+
+public class ProcessingTest extends PApplet{
+
+    @Override
+    public void settings() {
+        size(200, 200);
+    }
+
+    @Override
+    public void draw() {
+        background(0);
+        fill(255, 0, 0);
+        ellipse(100, 100, 100, 100);
+    }
+
+    public static void main (String[] args) {
+        ProcessingTest pt = new ProcessingTest();
+        PApplet.runSketch(new String[]{"ProcessingTest"}, pt);
+    }
+}
