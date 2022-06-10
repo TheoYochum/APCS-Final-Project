34de75a660609746ffe8a303192aaa8a6424022d
pharkoah <akram.kh.125@gmail.com>
2022-06-10 09:53:01 -0400

    works correctly

--- Project/test.sh
+++ Project/test.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-javac -cp ./core.jar ./ProcessingTest.java ProcessingTest.java && java-cp ./core.jar ./ProcessingTest.java ProcessingTest.java
+javac -cp ./core.jar ./ProcessingTest.java ProcessingTest.java && java -cp ./core.jar ./ProcessingTest.java ProcessingTest.java
