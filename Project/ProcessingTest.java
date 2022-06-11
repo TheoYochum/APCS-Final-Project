041142af2da5bebfaf603e425a03caf0603fa944
Theo Yochum <theoyochum@gmail.com>
2022-06-11 18:53:57 -0400

    Last commit not fully saved

--- Project/ProcessingTest.java
+++ Project/ProcessingTest.java
@@ -10,12 +10,14 @@ public class ProcessingTest extends PApplet{
     @Override
     public void settings() {
         size(800, 800);
+        Equation test = new Equation("2 * x ^ 2");
+        points = test.values(0, 800, 1, "x");
     }
 
     @Override
     public void draw() {
         strokeWeight(4);
-        graph(x, y);
+        graph(points);
     }
 
     public void graph(int[] x,  int[] y) {
@@ -24,6 +26,14 @@ public class ProcessingTest extends PApplet{
         }
     }
 
+    public void graph(List<Point> in) {
+        for (int i = 1; i < in.length(); i++) {
+            Point p1 = in.get(i - 1);
+            Point p2 = in.get(i);
+            line((float) p1.getX().value(), (float) p1.getY().value(), (float) p2.getX().value(), (float) p2.getY().value());
+        }
+    }
+ 
     public static void main (String[] args) {
         ProcessingTest pt = new ProcessingTest();
         PApplet.runSketch(new String[]{"ProcessingTest"}, pt);
