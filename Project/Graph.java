9d3f7df9197a1b61ebd80fa7cdb4a895d25f2018
Akram Khalifa <akram.kh.125@gmail.com>
2022-06-12 17:53:40 -0400

    can now take in two float arrays and graph their corresponding points

--- Project/Graph.java
+++ Project/Graph.java
@@ -5,32 +5,50 @@ import processing.core.PApplet;
 
 public class Graph extends PApplet{
 
-    List<Point> points;
-    Equation test;
+    List<Point> points, point;
+    Equation test, test2;
+    boolean list = false;
+    float[] xVals, yVals;
 
     public Graph(String exp) {
       test = new Equation(exp);
+      //test2 = new Equation("-1 * " + exp);
+      list = true;
+    }
+
+    public Graph(float[] x, float[] y) {
+      xVals = x;
+      yVals = y;
     }
 
     @Override
     public void settings() {
-        size(1000, 1000);
-        points = test.values(-width/2, width/2, 0.1, "x");
+        size(1800, 1000);
+        if (list) {
+          points = test.values(-width/2, width/2, 0.1, "x");
+        }
+        //point = test2.values(-width/2, width/2, 0.1, "x");
     }
 
     @Override
     public void draw() {
-      fill(255, 255, 0);
+        stroke(0);
         strokeWeight(2);
         line(width/2, 0, width/2, height);
         line(0, height/2, width, height/2);
         strokeWeight(4);
-        graph(points);
+        stroke(255, 0, 0);
+        if (list) {
+          graph(points);
+          //graph(point);
+        } else {
+          graph(xVals, yVals);
+        }
     }
 
-    public void graph(int[] x,  int[] y) {
+    public void graph(float[] x,  float[] y) {
         for (int i = 1; i < x.length; i++) {
-            line(x[i - 1], y[i - 1], x[i], y[i]);
+            line(x[i - 1] + width/2, height - y[i - 1] - height/2, x[i] + width/2, height - y[i] - height/2);
         }
     }
 
@@ -44,7 +62,11 @@ public class Graph extends PApplet{
     }
 
     public static void main (String[] args) {
-        Graph pt = new Graph("-1 * (sqrt(90 + x) * sqrt(90 - x) ) + 40");
-        PApplet.runSketch(new String[]{"Graph"}, pt);
+        //Graph pt = new Graph("((9 * sqrt(x + 36) * sqrt(x + 4) ) / 16) + 40");
+        float[] x = {100, 200, 300};
+        Graph pt1 = new Graph(x, x);
+        PApplet.runSketch(new String[]{"Graph"}, pt1);
+        //PApplet.runSketch(new String[]{"Graph"}, pt1);
+        // hyperbola right side: "((9 * sqrt(x + 36) * sqrt(x + 4) ) / 16) + 40"
     }
 }
